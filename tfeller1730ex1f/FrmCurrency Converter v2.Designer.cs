6e46cedff86342181afe95dfb6547bf0244c859e
Troy Feller <ID3434QP@go.minnstate.edu>
2019-09-29 09:28:33 -0500

    Completed ex1f

---
+++ tfeller1730ex1f/FrmCurrency Converter v2.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace tfeller1730ex1f
+{
+    partial class frmCurrency
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCurrency));
+            this.btnBritish = new System.Windows.Forms.Button();
+            this.picBritish = new System.Windows.Forms.PictureBox();
+            this.picMexicoDim = new System.Windows.Forms.PictureBox();
+            this.picMexico = new System.Windows.Forms.PictureBox();
+            this.picJapanDim = new System.Windows.Forms.PictureBox();
+            this.picJapan = new System.Windows.Forms.PictureBox();
+            this.picBritishDim = new System.Windows.Forms.PictureBox();
+            this.picEuro = new System.Windows.Forms.PictureBox();
+            this.picEuroDim = new System.Windows.Forms.PictureBox();
+            this.btnJapan = new System.Windows.Forms.Button();
+            this.btnMexico = new System.Windows.Forms.Button();
+            this.btnEuro = new System.Windows.Forms.Button();
+            this.lblCurrency = new System.Windows.Forms.Label();
+            this.txtCurrency = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRate = new System.Windows.Forms.TextBox();
+            this.txtUSDollars = new System.Windows.Forms.TextBox();
+            this.txtTotalUSD = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblEquation = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBritish)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMexicoDim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMexico)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picJapanDim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picJapan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBritishDim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picEuro)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picEuroDim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBritish
+            // 
+            this.btnBritish.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnBritish.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnBritish.Location = new System.Drawing.Point(12, 12);
+            this.btnBritish.Name = "btnBritish";
+            this.btnBritish.Size = new System.Drawing.Size(83, 76);
+            this.btnBritish.TabIndex = 1;
+            this.btnBritish.Text = "British Pound";
+            this.btnBritish.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnBritish.UseVisualStyleBackColor = true;
+            this.btnBritish.Click += new System.EventHandler(this.BtnBritish_Click);
+            // 
+            // picBritish
+            // 
+            this.picBritish.Image = ((System.Drawing.Image)(resources.GetObject("picBritish.Image")));
+            this.picBritish.Location = new System.Drawing.Point(402, 187);
+            this.picBritish.Name = "picBritish";
+            this.picBritish.Size = new System.Drawing.Size(51, 48);
+            this.picBritish.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBritish.TabIndex = 1;
+            this.picBritish.TabStop = false;
+            this.picBritish.Visible = false;
+            // 
+            // picMexicoDim
+            // 
+            this.picMexicoDim.Image = ((System.Drawing.Image)(resources.GetObject("picMexicoDim.Image")));
+            this.picMexicoDim.Location = new System.Drawing.Point(459, 77);
+            this.picMexicoDim.Name = "picMexicoDim";
+            this.picMexicoDim.Size = new System.Drawing.Size(51, 48);
+            this.picMexicoDim.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMexicoDim.TabIndex = 2;
+            this.picMexicoDim.TabStop = false;
+            this.picMexicoDim.Visible = false;
+            // 
+            // picMexico
+            // 
+            this.picMexico.Image = ((System.Drawing.Image)(resources.GetObject("picMexico.Image")));
+            this.picMexico.Location = new System.Drawing.Point(402, 77);
+            this.picMexico.Name = "picMexico";
+            this.picMexico.Size = new System.Drawing.Size(51, 48);
+            this.picMexico.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMexico.TabIndex = 3;
+            this.picMexico.TabStop = false;
+            this.picMexico.Visible = false;
+            // 
+            // picJapanDim
+            // 
+            this.picJapanDim.Image = ((System.Drawing.Image)(resources.GetObject("picJapanDim.Image")));
+            this.picJapanDim.Location = new System.Drawing.Point(459, 132);
+            this.picJapanDim.Name = "picJapanDim";
+            this.picJapanDim.Size = new System.Drawing.Size(51, 48);
+            this.picJapanDim.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picJapanDim.TabIndex = 4;
+            this.picJapanDim.TabStop = false;
+            this.picJapanDim.Visible = false;
+            // 
+            // picJapan
+            // 
+            this.picJapan.Image = ((System.Drawing.Image)(resources.GetObject("picJapan.Image")));
+            this.picJapan.Location = new System.Drawing.Point(402, 132);
+            this.picJapan.Name = "picJapan";
+            this.picJapan.Size = new System.Drawing.Size(51, 48);
+            this.picJapan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picJapan.TabIndex = 5;
+            this.picJapan.TabStop = false;
+            this.picJapan.Visible = false;
+            // 
+            // picBritishDim
+            // 
+            this.picBritishDim.Image = ((System.Drawing.Image)(resources.GetObject("picBritishDim.Image")));
+            this.picBritishDim.Location = new System.Drawing.Point(459, 187);
+            this.picBritishDim.Name = "picBritishDim";
+            this.picBritishDim.Size = new System.Drawing.Size(51, 48);
+            this.picBritishDim.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBritishDim.TabIndex = 6;
+            this.picBritishDim.TabStop = false;
+            this.picBritishDim.Visible = false;
+            // 
+            // picEuro
+            // 
+            this.picEuro.Image = ((System.Drawing.Image)(resources.GetObject("picEuro.Image")));
+            this.picEuro.Location = new System.Drawing.Point(402, 22);
+            this.picEuro.Name = "picEuro";
+            this.picEuro.Size = new System.Drawing.Size(51, 48);
+            this.picEuro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picEuro.TabIndex = 7;
+            this.picEuro.TabStop = false;
+            this.picEuro.Visible = false;
+            // 
+            // picEuroDim
+            // 
+            this.picEuroDim.Image = ((System.Drawing.Image)(resources.GetObject("picEuroDim.Image")));
+            this.picEuroDim.Location = new System.Drawing.Point(459, 22);
+            this.picEuroDim.Name = "picEuroDim";
+            this.picEuroDim.Size = new System.Drawing.Size(51, 48);
+            this.picEuroDim.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picEuroDim.TabIndex = 8;
+            this.picEuroDim.TabStop = false;
+            this.picEuroDim.Visible = false;
+            // 
+            // btnJapan
+            // 
+            this.btnJapan.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnJapan.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnJapan.Location = new System.Drawing.Point(101, 12);
+            this.btnJapan.Name = "btnJapan";
+            this.btnJapan.Size = new System.Drawing.Size(83, 76);
+            this.btnJapan.TabIndex = 2;
+            this.btnJapan.Text = "Japanese Yen";
+            this.btnJapan.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnJapan.UseVisualStyleBackColor = true;
+            this.btnJapan.Click += new System.EventHandler(this.BtnJapan_Click);
+            // 
+            // btnMexico
+            // 
+            this.btnMexico.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMexico.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnMexico.Location = new System.Drawing.Point(190, 12);
+            this.btnMexico.Name = "btnMexico";
+            this.btnMexico.Size = new System.Drawing.Size(83, 76);
+            this.btnMexico.TabIndex = 3;
+            this.btnMexico.Text = "Mexican Peso";
+            this.btnMexico.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnMexico.UseVisualStyleBackColor = true;
+            this.btnMexico.Click += new System.EventHandler(this.BtnMexico_Click);
+            // 
+            // btnEuro
+            // 
+            this.btnEuro.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnEuro.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnEuro.Location = new System.Drawing.Point(279, 12);
+            this.btnEuro.Name = "btnEuro";
+            this.btnEuro.Size = new System.Drawing.Size(87, 76);
+            this.btnEuro.TabIndex = 4;
+            this.btnEuro.Text = "European Euro";
+            this.btnEuro.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnEuro.UseVisualStyleBackColor = true;
+            this.btnEuro.Click += new System.EventHandler(this.BtnEuro_Click);
+            // 
+            // lblCurrency
+            // 
+            this.lblCurrency.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lblCurrency.Location = new System.Drawing.Point(9, 117);
+            this.lblCurrency.Name = "lblCurrency";
+            this.lblCurrency.Size = new System.Drawing.Size(100, 23);
+            this.lblCurrency.TabIndex = 0;
+            // 
+            // txtCurrency
+            // 
+            this.txtCurrency.Location = new System.Drawing.Point(115, 120);
+            this.txtCurrency.Name = "txtCurrency";
+            this.txtCurrency.Size = new System.Drawing.Size(100, 20);
+            this.txtCurrency.TabIndex = 5;
+            this.txtCurrency.Text = "0.00";
+            this.txtCurrency.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtCurrency.Click += new System.EventHandler(this.TxtCurrency_Enter);
+            this.txtCurrency.TextChanged += new System.EventHandler(this.calcUSD);
+            this.txtCurrency.Enter += new System.EventHandler(this.TxtCurrency_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rate:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "$US";
+            // 
+            // txtRate
+            // 
+            this.txtRate.Location = new System.Drawing.Point(115, 149);
+            this.txtRate.Name = "txtRate";
+            this.txtRate.Size = new System.Drawing.Size(100, 20);
+            this.txtRate.TabIndex = 6;
+            this.txtRate.Text = "0.00";
+            this.txtRate.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtRate.TextChanged += new System.EventHandler(this.calcUSD);
+            // 
+            // txtUSDollars
+            // 
+            this.txtUSDollars.Location = new System.Drawing.Point(115, 175);
+            this.txtUSDollars.Name = "txtUSDollars";
+            this.txtUSDollars.ReadOnly = true;
+            this.txtUSDollars.Size = new System.Drawing.Size(100, 20);
+            this.txtUSDollars.TabIndex = 0;
+            this.txtUSDollars.TabStop = false;
+            this.txtUSDollars.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtTotalUSD
+            // 
+            this.txtTotalUSD.Location = new System.Drawing.Point(115, 201);
+            this.txtTotalUSD.Name = "txtTotalUSD";
+            this.txtTotalUSD.ReadOnly = true;
+            this.txtTotalUSD.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalUSD.TabIndex = 0;
+            this.txtTotalUSD.TabStop = false;
+            this.txtTotalUSD.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(115, 227);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(100, 23);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(221, 227);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 23);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(221, 172);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(30, 23);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // lblEquation
+            // 
+            this.lblEquation.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lblEquation.Location = new System.Drawing.Point(9, 198);
+            this.lblEquation.Name = "lblEquation";
+            this.lblEquation.Size = new System.Drawing.Size(100, 52);
+            this.lblEquation.TabIndex = 0;
+            // 
+            // frmCurrency
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 262);
+            this.Controls.Add(this.lblEquation);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtTotalUSD);
+            this.Controls.Add(this.txtUSDollars);
+            this.Controls.Add(this.txtRate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCurrency);
+            this.Controls.Add(this.lblCurrency);
+            this.Controls.Add(this.btnEuro);
+            this.Controls.Add(this.btnMexico);
+            this.Controls.Add(this.btnJapan);
+            this.Controls.Add(this.picEuroDim);
+            this.Controls.Add(this.picEuro);
+            this.Controls.Add(this.picBritishDim);
+            this.Controls.Add(this.picJapan);
+            this.Controls.Add(this.picJapanDim);
+            this.Controls.Add(this.picMexico);
+            this.Controls.Add(this.picMexicoDim);
+            this.Controls.Add(this.picBritish);
+            this.Controls.Add(this.btnBritish);
+            this.Name = "frmCurrency";
+            this.Text = "tfeller1730ex1f: Currency Converter v2";
+            this.Load += new System.EventHandler(this.FrmCurrency_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBritish)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMexicoDim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMexico)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picJapanDim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picJapan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBritishDim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picEuro)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picEuroDim)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBritish;
+        private System.Windows.Forms.PictureBox picBritish;
+        private System.Windows.Forms.PictureBox picMexicoDim;
+        private System.Windows.Forms.PictureBox picMexico;
+        private System.Windows.Forms.PictureBox picJapanDim;
+        private System.Windows.Forms.PictureBox picJapan;
+        private System.Windows.Forms.PictureBox picBritishDim;
+        private System.Windows.Forms.PictureBox picEuro;
+        private System.Windows.Forms.PictureBox picEuroDim;
+        private System.Windows.Forms.Button btnJapan;
+        private System.Windows.Forms.Button btnMexico;
+        private System.Windows.Forms.Button btnEuro;
+        private System.Windows.Forms.Label lblCurrency;
+        private System.Windows.Forms.TextBox txtCurrency;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRate;
+        private System.Windows.Forms.TextBox txtUSDollars;
+        private System.Windows.Forms.TextBox txtTotalUSD;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblEquation;
+    }
+}
+
---
+++ tfeller1730ex1f/FrmCurrency Converter v2.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tfeller1730ex1f
+{
+    public partial class frmCurrency : Form
+    {
+        public frmCurrency()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmCurrency_Load(object sender, EventArgs e)
+        {
+            btnBritish.BackgroundImage = picBritish.Image;
+            btnJapan.BackgroundImage = picJapanDim.Image;
+            btnMexico.BackgroundImage = picMexicoDim.Image;
+            btnEuro.BackgroundImage = picEuroDim.Image;
+            txtRate.Text = "1.22926";
+            txtCurrency.Text = "0.00";
+            txtUSDollars.Text = "0.00";
+            txtTotalUSD.Text = "0.00";
+            lblCurrency.Text = btnBritish.Text + ": ";
+            txtCurrency.Focus();
+        }
+
+        private void BtnBritish_Click(object sender, EventArgs e)
+        {
+            btnBritish.BackgroundImage = picBritish.Image;
+            btnJapan.BackgroundImage = picJapanDim.Image;
+            btnMexico.BackgroundImage = picMexicoDim.Image;
+            btnEuro.BackgroundImage = picEuroDim.Image;
+            txtRate.Text = "1.22926";
+            txtCurrency.Text = "0.00";
+            txtUSDollars.Text = "0.00";
+            lblCurrency.Text = btnBritish.Text + ": ";
+            txtCurrency.Focus();
+            txtTotalUSD.Text = txtTotalUSD.Text;
+        }
+
+        private void BtnJapan_Click(object sender, EventArgs e)
+        {
+            btnJapan.BackgroundImage = picJapan.Image;
+            btnBritish.BackgroundImage = picBritishDim.Image;
+            btnMexico.BackgroundImage = picMexicoDim.Image;
+            btnEuro.BackgroundImage = picEuroDim.Image;
+            txtRate.Text = "0.00926300";
+            txtCurrency.Text = "0.00";
+            txtUSDollars.Text = "0.00";
+            lblCurrency.Text = btnJapan.Text + ": ";
+            txtCurrency.Focus();
+            txtTotalUSD.Text = txtTotalUSD.Text;
+        }
+
+        private void BtnMexico_Click(object sender, EventArgs e)
+        {
+            btnMexico.BackgroundImage = picMexico.Image;
+            btnJapan.BackgroundImage = picJapanDim.Image;
+            btnBritish.BackgroundImage = picBritishDim.Image;
+            btnEuro.BackgroundImage = picEuroDim.Image;
+            txtRate.Text = "0.0507815";
+            txtCurrency.Text = "0.00";
+            txtUSDollars.Text = "0.00";
+            lblCurrency.Text = btnMexico.Text + ": ";
+            txtCurrency.Focus();
+            txtTotalUSD.Text = txtTotalUSD.Text;
+        }
+
+        private void BtnEuro_Click(object sender, EventArgs e)
+        {
+            btnEuro.BackgroundImage = picEuro.Image;
+            btnJapan.BackgroundImage = picJapanDim.Image;
+            btnMexico.BackgroundImage = picMexicoDim.Image;
+            btnBritish.BackgroundImage = picBritishDim.Image;
+            txtRate.Text = "1.09410";
+            txtCurrency.Text = "0.00";
+            txtUSDollars.Text = "0.00";
+            lblCurrency.Text = btnEuro.Text + ": ";
+            txtCurrency.Focus();
+            txtTotalUSD.Text = txtTotalUSD.Text;
+        }
+
+        private void calcUSD(object sender, EventArgs e)
+        {
+            txtUSDollars.Text = (
+                Convert.ToDecimal(txtCurrency.Text) * Convert.ToDecimal(txtRate.Text)
+                ).ToString("0.00");
+        }
+
+        private void TxtCurrency_Enter(object sender, EventArgs e)
+        {
+            txtCurrency.SelectAll();
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            lblEquation.Text = lblEquation.Text + " + " + txtUSDollars.Text;
+
+            txtTotalUSD.Text = (
+                Convert.ToDecimal(txtTotalUSD.Text) + Convert.ToDecimal(txtUSDollars.Text)
+                ).ToString("0.00");
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            btnBritish.BackgroundImage = picBritish.Image;
+            btnJapan.BackgroundImage = picJapanDim.Image;
+            btnMexico.BackgroundImage = picMexicoDim.Image;
+            btnEuro.BackgroundImage = picEuroDim.Image;
+            txtRate.Text = "1.22926";
+            txtCurrency.Text = "0.00";
+            txtUSDollars.Text = "0.00";
+            lblCurrency.Text = btnBritish.Text + ": ";
+            txtCurrency.Focus();
+            txtTotalUSD.Text = "0.00";
+            lblEquation.Text = " ";
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
